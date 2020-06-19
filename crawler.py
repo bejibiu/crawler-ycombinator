ac99c813909abc8a7bc8e595f19372593a6e4a3f
6ejibiu <akimovpnz@live.com>
2020-06-20 00:35:15 +0300

    decompose func many request.

--- crawler.py
+++ crawler.py
@@ -2,6 +2,7 @@ import argparse
 import asyncio
 import logging
 import os
+import re
 
 import aiohttp
 import async_timeout
@@ -10,6 +11,7 @@ from aiofile import AIOFile
 from bs4 import BeautifulSoup
 
 SITE_NEWS = "https://news.ycombinator.com"
+# SITE_NEWS = "http://ycombinator.com"
 NEWS_DETAILS_URL = SITE_NEWS + "/item?id={id_news}"
 # NEWS_DETAILS_URL = SITE_NEWS + "/{id_news}"
 BASE_DIR = os.path.dirname(__file__)
@@ -23,9 +25,7 @@ async def download_one(session, url, encoded=True):
                 if encoded:
                     return await response.text()
                 return await response.read()
-    except (aiohttp.client_exceptions.ClientConnectionError, UnicodeDecodeError,) as e:
-        logging.error(f"can not load {url}. Error = {e}")
-    except asyncio.exceptions.TimeoutError:
+    except asyncio.TimeoutError:
         logging.error(f"url {url} not avalible.")
     except Exception as e:
         logging.error(f"Other error {url}. Error: {e} ")
@@ -41,48 +41,60 @@ async def save_text(path_to_save, text):
         await f.write(text)
 
 
+def slugify(text):
+    return re.sub(r'[\W_]+', '-', text)
+
+
 async def fetch(session, url, semaphore):
     async with semaphore:
         news_details = await download_one(session, url)
         if not news_details:
             logging.error(f"News on site {news_details}, not download")
             return None
+
         soup = BeautifulSoup(news_details, "html.parser")
-        news_site_block = soup.find("a", {"class": "storylink"})
-        if not news_site_block:
-            logging.error(f"Not found site for this news: {url}")
-        news_site = news_site_block["href"]
-        news_name = news_site_block.text.translate(
-            str.maketrans("\_-/:$!@#$", "          ")
-        )
+
+        news_name = slugify(soup.find("a", {"class": "storylink"}).text)
+
         await save_text(
-            os.path.join(RESULT_DIR, news_name, "news_details.html"),
+            os.path.join(RESULT_DIR, news_name if news_name else url.split("=")[-1], "news_details.html"),
             news_details.encode(),
         )
-        all_links = []
-        commnet_tree = soup.find("table", {"class": "comment-tree"})
-        if soup.find("table", {"class": "comment-tree"}):
-            all_links = [
-                link["href"]
-                for link in commnet_tree.find_all("a")
-                if link["href"].startswith("https://")
-            ]
-        logging.info(f"Found {len(all_links)} comment for news {news_name}")
-        try:
-            news_text = await download_one(session, news_site, encoded=False)
-        except asyncio.exceptions.TimeoutError:
-            logging.error(f"url {news_site} not avalible.")
-            news_text = f"Sorry =(. Url {news_site} not avalible.".encode()
-        await save_text(os.path.join(RESULT_DIR, news_name, news_name), news_text)
-        comments_news = [
-            asyncio.create_task(download_one(session, url_from_comment, encoded=False))
-            for url_from_comment in all_links
-        ]
-        comments_list_text = await asyncio.gather(*comments_news)
-        for num, i in enumerate(comments_list_text):
-            if i:
-                await save_text(os.path.join(RESULT_DIR, news_name, f"comment{num}"), i)
+        if news_name:
+            await download_theme_news(news_name, session, soup)
+        await download_link_from_comments(news_name, session, soup)
         logging.info(f"news {news_name} was full download ")
+
+
+async def download_link_from_comments(news_name, session, soup):
+    all_links = []
+    comment_tree = soup.find("table", {"class": "comment-tree"})
+    if soup.find("table", {"class": "comment-tree"}):
+        all_links = [
+            link["href"]
+            for link in comment_tree.find_all("a")
+            if link["href"].startswith("https://")
+        ]
+    logging.info(f"Found {len(all_links)} comment for news {news_name}")
+
+    comments_news = [
+        asyncio.create_task(download_one(session, url_from_comment, encoded=False))
+        for url_from_comment in all_links
+    ]
+    comments_list_text = await asyncio.gather(*comments_news)
+    for num, i in enumerate(comments_list_text):
+        if i:
+            await save_text(os.path.join(RESULT_DIR, news_name, f"comment{num}"), i)
+
+
+async def download_theme_news(news_name, session, soup):
+    news_site_block = soup.find("a", {"class": "storylink"})
+    news_site = news_site_block["href"]
+    news_text = await download_one(session, news_site, encoded=False)
+    if not news_text:
+        logging.error(f"url {news_site} not avalible.")
+        news_text = f"Sorry =(. Url {news_site} not avalible.".encode()
+    await save_text(os.path.join(RESULT_DIR, news_name, news_name), news_text)
 
 
 async def main(args, list_news):
@@ -108,6 +120,13 @@ async def main(args, list_news):
     print("This all")
 
 
+async def run_forever(args, list_news):
+    while True:
+        logging.info("Run chiclefetche news")
+        await main(args, list_news)
+        await asyncio.sleep(args.period)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -117,7 +136,8 @@ if __name__ == "__main__":
         help="result output folder",
     )
     parser.add_argument(
-        "-p", "--period", default=60 * 5, help="period renew site to download",
+        "-p", "--period", default=60 * 5, type=int,
+        help="period renew site to download",
     )
     parser.add_argument(
         "--timeout",
@@ -145,11 +165,5 @@ if __name__ == "__main__":
     )
     if not os.path.exists(RESULT_DIR):
         os.mkdir(RESULT_DIR)
-    import datetime
-
-    now = datetime.datetime.now()
     list_news = []
-    # while True:
-    asyncio.run(main(args, list_news))
-    # import time; time.sleep(3)
-    print(datetime.datetime.now() - now)
+    asyncio.run(run_forever(args, list_news))
