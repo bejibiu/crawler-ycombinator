f9613366855fe6d2881202228cefa98fad28359a
6ejibiu <akimovpnz@live.com>
2020-06-20 18:24:58 +0300

    add args to contlos apps

--- crawler.py
+++ crawler.py
@@ -5,26 +5,21 @@ import os
 import re
 
 import aiohttp
-import async_timeout
 
 from aiofile import AIOFile
 from bs4 import BeautifulSoup
 
 SITE_NEWS = "https://news.ycombinator.com"
-# SITE_NEWS = "http://ycombinator.com"
 NEWS_DETAILS_URL = SITE_NEWS + "/item?id={id_news}"
-# NEWS_DETAILS_URL = SITE_NEWS + "/{id_news}"
 BASE_DIR = os.path.dirname(__file__)
-RESULT_DIR = os.path.join(BASE_DIR, "output")
 
 
 async def download_one(session, url, encoded=True):
     try:
-        with async_timeout.timeout(10):
-            async with session.get(url) as response:
-                if encoded:
-                    return await response.text()
-                return await response.read()
+        async with session.get(url) as response:
+            if encoded:
+                return await response.text()
+            return await response.read()
     except asyncio.TimeoutError:
         logging.error(f"url {url} not avalible.")
     except Exception as e:
@@ -42,10 +37,10 @@ async def save_text(path_to_save, text):
 
 
 def slugify(text):
-    return re.sub(r'[\W_]+', '-', text)
+    return re.sub(r"[\W_]+", "-", text)
 
 
-async def fetch(session, url, semaphore):
+async def fetch(output_folder, session, url, semaphore):
     async with semaphore:
         news_details = await download_one(session, url)
         if not news_details:
@@ -57,23 +52,27 @@ async def fetch(session, url, semaphore):
         news_name = slugify(soup.find("a", {"class": "storylink"}).text)
 
         await save_text(
-            os.path.join(RESULT_DIR, news_name if news_name else url.split("=")[-1], "news_details.html"),
+            os.path.join(
+                output_folder,
+                news_name if news_name else url.split("=")[-1],
+                "news_details.html",
+            ),
             news_details.encode(),
         )
         if news_name:
-            await download_theme_news(news_name, session, soup)
-        await download_link_from_comments(news_name, session, soup)
+            await download_theme_news(output_folder, news_name, session, soup)
+        await download_link_from_comments(output_folder, news_name, session, soup)
         logging.info(f"news {news_name} was full download ")
 
 
-async def download_link_from_comments(news_name, session, soup):
+async def download_link_from_comments(output_folder, news_name, session, soup):
     all_links = []
     comment_tree = soup.find("table", {"class": "comment-tree"})
     if soup.find("table", {"class": "comment-tree"}):
         all_links = [
             link["href"]
             for link in comment_tree.find_all("a")
-            if link["href"].startswith("https://")
+            if link["href"].startswith("https://") or link["href"].startswith("http://")
         ]
     logging.info(f"Found {len(all_links)} comment for news {news_name}")
 
@@ -84,22 +83,26 @@ async def download_link_from_comments(news_name, session, soup):
     comments_list_text = await asyncio.gather(*comments_news)
     for num, i in enumerate(comments_list_text):
         if i:
-            await save_text(os.path.join(RESULT_DIR, news_name, f"comment{num}"), i)
+            await save_text(os.path.join(output_folder, news_name, f"comment{num}"), i)
 
 
-async def download_theme_news(news_name, session, soup):
+async def download_theme_news(output_folder, news_name, session, soup):
     news_site_block = soup.find("a", {"class": "storylink"})
     news_site = news_site_block["href"]
+    if not news_site.startswith("https://") and not news_site.startswith("http://"):
+        logging.info("News text locate in details")
+        return None
     news_text = await download_one(session, news_site, encoded=False)
     if not news_text:
         logging.error(f"url {news_site} not avalible.")
         news_text = f"Sorry =(. Url {news_site} not avalible.".encode()
-    await save_text(os.path.join(RESULT_DIR, news_name, news_name), news_text)
+    await save_text(os.path.join(output_folder, news_name, news_name), news_text)
 
 
 async def main(args, list_news):
-    semaphore = asyncio.Semaphore(2)
-    async with aiohttp.ClientSession() as session:
+    semaphore = asyncio.Semaphore(args.semaphore)
+    timeout = aiohttp.ClientTimeout(total=args.timeout)
+    async with aiohttp.ClientSession(timeout=timeout) as session:
         res = await download_one(session, SITE_NEWS)
         soup = BeautifulSoup(res, "html.parser")
         list_tasks_fetch_news = []
@@ -112,7 +115,10 @@ async def main(args, list_news):
             list_tasks_fetch_news.append(
                 asyncio.create_task(
                     fetch(
-                        session, NEWS_DETAILS_URL.format(id_news=news["id"]), semaphore
+                        args.output,
+                        session,
+                        NEWS_DETAILS_URL.format(id_news=news["id"]),
+                        semaphore,
                     )
                 )
             )
@@ -132,12 +138,18 @@ if __name__ == "__main__":
     parser.add_argument(
         "-o",
         "--output",
-        default=os.path.join(BASE_DIR, "result"),
+        default=os.path.join(BASE_DIR, "output"),
         help="result output folder",
     )
     parser.add_argument(
-        "-p", "--period", default=60 * 5, type=int,
+        "-p",
+        "--period",
+        default=60 * 5,
+        type=int,
         help="period renew site to download",
+    )
+    parser.add_argument(
+        "--semaphore", default=2, type=int, help="number of simultaneous connections",
     )
     parser.add_argument(
         "--timeout",
@@ -163,7 +175,7 @@ if __name__ == "__main__":
         format="[%(asctime)s] %(threadName)s %(levelname).1s %(message)s",
         datefmt="%Y.%m.%d %H:%M:%S",
     )
-    if not os.path.exists(RESULT_DIR):
-        os.mkdir(RESULT_DIR)
+    if not os.path.exists(args.output):
+        os.mkdir(args.output)
     list_news = []
     asyncio.run(run_forever(args, list_news))
